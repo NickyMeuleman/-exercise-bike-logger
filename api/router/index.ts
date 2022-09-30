bd28626efba4986398d388a8338603dbbaefe547
Nicky Meuleman <nicky.dev@outlook.com>
2022-09-30 21:50:35 +0200

    start home page data filtering

--- api/router/index.ts
+++ api/router/index.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 import { t } from "../context";
 import { exampleRouter } from "./example";
+import { ritRouter } from "./rit";
 
 export const appRouter = t.router({
   example: exampleRouter,
+  rit: ritRouter,
   greeting: t.procedure
     .input(z.object({ name: z.string() }).nullish())
     .query(({ input }) => {
