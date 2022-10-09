6d05f19a7801fd955b3b4d8f58de14b146a6b8c4
Nicky Meuleman <nicky.dev@outlook.com>
2022-10-09 23:23:09 +0200

    distance stepsize 10m

--- renderer/src/components/RitForm.tsx
+++ renderer/src/components/RitForm.tsx
@@ -183,7 +183,7 @@ export const RitForm: React.FC<{
                         shadow-sm
                         focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
                         "
-                  step={0.1}
+                  step={0.01}
                   {...field}
                   value={
                     fieldState.isDirty
